bb95c4a75476f57f84b203e915910929870168a6
Pawan Prakash Sharma <pawan.sharma@wissen.com>
2021-07-09 16:20:42 +0530

    commit done by Pawan

---
+++ src/main/java/com/wissen/junitdemo/CalculatorList.java
@@ -0,0 +1,13 @@
+package com.wissen.junitdemo;
+
+import java.util.ArrayList;
+
+public class CalculatorList {
+
+	ArrayList<Integer> list = new ArrayList<>();
+	
+	public void addElement(int a) {
+		list.add(a);
+	}
+	
+}
